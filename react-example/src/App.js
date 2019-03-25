fdfdbfefc4cef01b76b4351ce1170d091166c0dd
ParkChunSo <cnsth123@gmail.com>
2019-03-25 21:32:25 +0900

    init

---
+++ react-example/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Contact from './component/Contact';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div><Contact/></div>
+    );
+  }
+}
+
+export default App;
